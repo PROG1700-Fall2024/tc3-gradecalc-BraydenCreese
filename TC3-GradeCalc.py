35e89da995846ff1003930d17bd6c17c40292394
BraydenCreese <kidson2017@gmail.com>
2024-10-21 09:35:50 -0700

    Done.

--- TC3-GradeCalc.py
+++ TC3-GradeCalc.py
@@ -17,11 +17,74 @@
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    # Gives user a breakdown for the prog
+    print("Grade Point Calculator")
+    print("\nValid letter grades that can be entered: A, B, C, D,F.")
+    print("Valid grade modifiers are +, - or nothing.")
+    print("All letter grades except F can include a + or - symbol")
+    print("Calculated grade point value cannot exceed 4.0.")
 
 
+    # gets user input and catches for any invalid inputs. ( Takes all input as uppercase Ex: 'A' )
+    while True:
+            userLetterGrade = input("\nPlease enter a letter grade: ").upper()
+            if userLetterGrade in ['A', 'B', 'C', 'D', 'F']:
+                  break
+            else:
+                  print("Invalid Input! Please enter a valid letter grade.")
 
 
+    while True:
+        gradeModifier = input("Please enter a modifier (+, - or nothing): ")
+        if gradeModifier in ['+', '-', '']:
+              break
+        else:
+            print("Invalid Input! Please enter a valid letter grade.")
 
+
+    # using if statmenats to check the grade and modifier entered by the user
+    """
+    desided to do this in a simple way this time, we could make a table and make a table and map out the values
+    Ex: ( gradePoints = {'A': 4.0, 'B': 3.0, 'C': 2.0, 'D': 1.0, 'F': 0.0} ). Then use this to make a few
+    if, else, elif statments that check for the gradeModifier and userLetterGrade, add a check for a A+ cap at
+    4.0 and a low cap for no F+ or F-, Then a small check for the -/+ for the 0.3 grade point diff.
+    """
+    if userLetterGrade == 'A':
+        if gradeModifier == '-':
+            numValue = 3.7
+        else:
+            numValue = 4.0
+
+    elif userLetterGrade == 'B':
+        if gradeModifier == '+':
+            numValue = 3.3
+        elif gradeModifier == '-':
+            numValue = 2.7
+        else:
+            numValue = 3.0
+
+    elif userLetterGrade == 'C':
+        if gradeModifier == '+':
+            numValue = 2.3
+        elif gradeModifier == '-':
+            numValue = 1.7
+        else:
+            numValue = 2.0
+
+    elif userLetterGrade == 'D':
+        if gradeModifier == '+':
+            numValue = 1.3
+        elif gradeModifier == '-':
+            numValue = 0.7
+        else:
+            numValue = 1.0
+    
+    else:
+        numValue = 0.0
+
+
+    # displaying the users output from the inputed values
+    print(f"\nThe numeric value for {userLetterGrade}{gradeModifier} is {numValue:.1f}.")
 
 
     # YOUR CODE ENDS HERE
